ad409412f8156bcd2016f170ce8d3e8fcbad06a1
Iyyappan P.A <iyyappan98@example.com>
2026-02-24 19:44:32 +0530

    backend bravo api value sending axios

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const nodemailer = require("nodemailer");
+const axios = require("axios");
 const EMAIL_TEMPLATE_CONTACT = require("../utils/emailTemplate");
 
 router.post("/", async (req, res) => {
@@ -8,38 +8,56 @@ router.post("/", async (req, res) => {
     const { name, email, message } = req.body;
 
     if (!name || !email || !message) {
-      return res.status(400).json({ success: false, message: "All fields required" });
+      return res.status(400).json({
+        success: false,
+        message: "All fields required",
+      });
     }
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
 
     const finalHtml = EMAIL_TEMPLATE_CONTACT
       .replace(/{{name}}/g, name)
-      .replace(/{{email}}/g, email)      
-      .replace(/{{message}}/g, message)
-      
+      .replace(/{{email}}/g, email)
+      .replace(/{{message}}/g, message);
 
-    const mailOptions = {
-      from: process.env.SENDER_EMAIL,
-      to: process.env.ADMIN_EMAIL,
-      replyTo: email,
-      subject: `New Enquiry Contact - ${name}`,
-      html: finalHtml,
-    };
+    await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Uma Cab Contact",
+          email: process.env.SENDER_EMAIL,
+        },
+        to: [
+          {
+            email: process.env.ADMIN_EMAIL,
+          },
+        ],
+        replyTo: {
+          email: email,
+          name: name,
+        },
+        subject: `New Enquiry Contact - ${name}`,
+        htmlContent: finalHtml,
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    await transporter.sendMail(mailOptions);
+    res.json({
+      success: true,
+      message: "Message sent successfully",
+    });
 
-    res.json({ success: true, message: "Message sent successfully" });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: "Mail not sent" });
+    console.log(error.response?.data || error.message);
+
+    res.status(500).json({
+      success: false,
+      message: "Mail not sent",
+    });
   }
 });
 
