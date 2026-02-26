736b64be458dba55b7211f30381d7fbda853f094
Iyyappan P.A <iyyappan98@example.com>
2026-02-26 20:01:15 +0530

    vercel link backend cors added

--- server.js
+++ server.js
@@ -4,8 +4,23 @@ require("dotenv").config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://umacabiyyappan.vercel.app/"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
 
 app.use(express.json());
