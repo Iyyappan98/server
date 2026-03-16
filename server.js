629cbe03de205a391edbc0def86d00c624eb20aa
Iyyappan P.A <iyyappan98@example.com>
2026-03-16 20:36:23 +0530

    fix cors for umacab domain

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://umacabiyyappan.vercel.app"
+  "https://umacabiyyappan.vercel.app",
+  "https://umacab.com",
+  "https://www.umacab.com"
 ];
 
 app.use(cors({
